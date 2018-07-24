8c66688c3518976a1ef96b7d1a913b6a18691d18
hugobex <hugobex@note-dell.note-dell>
2018-07-24 02:42:31 -0300

    Ajuste no Test Suite

--- ProjetoMatrix/src/test/java/br/com/cariocadev/ProjetoMatrix/AppTest.java
+++ ProjetoMatrix/src/test/java/br/com/cariocadev/ProjetoMatrix/AppTest.java
@@ -1,38 +1,12 @@
 package br.com.cariocadev.ProjetoMatrix;
 
-import junit.framework.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AvaliadorAlunoTest.class, CalculadoraSalarioTest.class })
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
