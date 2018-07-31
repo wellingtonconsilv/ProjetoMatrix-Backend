c72de279a964b07be800466cd7606fc1ee1b1c6d
wellingtonconsilv <wellingtonconsilv@gmail.com>
2018-07-31 01:58:02 -0300

    Correções e melhorias.

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
@@ -1,36 +1,33 @@
 package br.com.cariocadev.ProjetoMatrix;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
 import java.math.BigDecimal;
 
 public class AvaliadorAluno {
 
 	public BigDecimal getMaiorNota(BigDecimal[] notas) {
 		
-		 BigDecimal maior = new BigDecimal("0");
+		if(notas != null) {
+			
+		 List<BigDecimal> maiorNotaAluno = Arrays.asList(notas);
+		 Collections.sort(maiorNotaAluno, Collections.reverseOrder());
 		 
-		 if(notas != null) {
-			 for (BigDecimal nota: notas){
-				 if(nota.compareTo(maior)==1){
-		    		 maior = nota;
-		         }
-		     }
-		     return maior.setScale(2);
-		 }else {
-			  throw new IllegalArgumentException();
-		 }
+		 return maiorNotaAluno.get(0);
+		 
+		}else {
+			throw new IllegalArgumentException();
+		}
 		 
 	}
 
 	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
+				
+		if(validarNota(nota1) && validarNota(nota2) && validarNota(nota3)) {
 		
-		BigDecimal media = new BigDecimal("0");
-		
-		if((nota1!=null && nota2!=null && nota3 != null) &&
-          (nota1.compareTo(new BigDecimal("0"))>0 && nota2.compareTo(new BigDecimal(0))>0 && nota3.compareTo(new BigDecimal(0))>0) &&
-          (nota1.compareTo(new BigDecimal("10"))<=0 && nota2.compareTo(new BigDecimal("10"))<=0 && nota3.compareTo(new BigDecimal("10"))<=0)) {
-		
-			media = nota1.add(nota2).add(nota3).divide(new BigDecimal (3), 2, BigDecimal.ROUND_HALF_UP);
-	
+			BigDecimal media = nota1.add(nota2).add(nota3).divide(BigDecimal.valueOf(3), 2, BigDecimal.ROUND_HALF_UP);
+			
 	        return media.setScale(2);
 		 }else {
 			 throw new IllegalArgumentException();
@@ -39,25 +36,22 @@ public class AvaliadorAluno {
 	}
 
 	public String getStatus(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		  BigDecimal media = new BigDecimal("0");
-	        if((nota1!=null && nota2!=null && nota3 != null) &&
-	          (nota1.compareTo(new BigDecimal("0"))>0 && nota2.compareTo(new BigDecimal(0))>0 && nota3.compareTo(new BigDecimal(0))>0) &&
-	          (nota1.compareTo(new BigDecimal("10"))<=0 && nota2.compareTo(new BigDecimal("10"))<=0 && nota3.compareTo(new BigDecimal("10"))<=0)) {
 
-	            media = getMedia(nota1, nota2, nota3);
+	            BigDecimal media = getMedia(nota1, nota2, nota3);
 
-	            if (media.compareTo(new BigDecimal("6")) >= 0) {
+	            if (media.compareTo(BigDecimal.valueOf(6)) >= 0) {
 	                return "APROVADO";
-	            }else if (media.compareTo(new BigDecimal("4")) >= 0 && media.compareTo(new BigDecimal("6")) < 0) {
+	            }else if (media.compareTo(BigDecimal.valueOf(4)) >= 0 && media.compareTo(BigDecimal.valueOf(6)) < 0) {
 	                return "PROVA_FINAL";
 	            }else {
 	                return "REPROVADO";
 	            }
-	        }else {
-	            throw new IllegalArgumentException();
-	        }
+	}
+	
+	public boolean validarNota(BigDecimal nota) {		
+		
+		return nota!=null && nota.compareTo(BigDecimal.ZERO) >= 0 && nota.compareTo(BigDecimal.TEN) <= 0;
+		
 	}
 
 }
-
-
